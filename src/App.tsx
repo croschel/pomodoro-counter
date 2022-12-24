5bedced94c7f694405f8323432699a4be3cdd493
croschel <croschel000@gmail.com>
2022-12-24 13:50:00 -0300

    initial commit structure with styled component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./Button.style";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/theme/default";
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/Button.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  width: 70px;
+  height: 40px;
+  background-color: ${(props) => props.theme["green-500"]};
+`;
